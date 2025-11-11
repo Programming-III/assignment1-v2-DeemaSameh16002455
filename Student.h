6fda036307cb636aca3286681a5f099a91105287
DeemaSameh16002455 <deemas.mahmoud@gmail.com>
2025-11-11 09:17:32 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -6,7 +6,21 @@
 #using namespace std;
 //#write student class here
 
+class Student: public Person{
+private:
+int yearLevel; 
+string major;
+public:
+Student();
+Student (int y, string m, string n, int Pid): Person (n, Pid);
+~Student();
+void display();
+void setYearLevel(int newYear);
+void setMajor(string newMajor);
+int getYearLevel();
+string getMajor();
 
+};
 
 
 
