2535e56ef7cfdf478004d87ad5309006ec38eb00
DeemaSameh16002455 <deemas.mahmoud@gmail.com>
2025-11-11 09:26:08 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -5,7 +5,20 @@
 using namespace std;
 //#write Instructor class here
 
-
+class Instructor: public Person {
+private:
+string department; 
+int experienceYears;
+public:
+Instructor();
+Instructor(string d, int exp, string n, int Pid): Person (n, Pid); 
+~Instructor(); 
+void diplay();
+void setDepartment (string newDep); 
+void setExp (int newExp);
+string getDep();
+int getExp();
+};
 
 
 
